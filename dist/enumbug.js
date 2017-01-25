fc0f7f2f6627a46ce7737949706ceaf3cb735b26
Maxim Devoir <maximdevoir@hotmail.com>
2017-01-25 10:55:51 -0800

    Build 0.11.0

--- dist/enumbug.js
+++ dist/enumbug.js
@@ -1,1 +1,27 @@
-!function(root,factory){"object"==typeof exports&&"object"==typeof module?module.exports=factory():"function"==typeof define&&define.amd?define("enumBug",[],factory):"object"==typeof exports?exports.enumBug=factory():root.enumBug=factory()}(this,function(){return function(modules){function __webpack_require__(moduleId){if(installedModules[moduleId])return installedModules[moduleId].exports;var module=installedModules[moduleId]={exports:{},id:moduleId,loaded:!1};return modules[moduleId].call(module.exports,module,module.exports,__webpack_require__),module.loaded=!0,module.exports}var installedModules={};return __webpack_require__.m=modules,__webpack_require__.c=installedModules,__webpack_require__.p="",__webpack_require__(0)}([function(module,exports){"use strict";function enumBug(obj,callback){if(hasEnumBug)for(var i=0;i<enumBugsLength;i+=1){var prop=enumBugProps[i];hasOwnProperty.call(obj,prop)&&callback.call(void 0,prop)}}var enumBugProps=["constructor","hasOwnProperty","isPrototypeOf","propertyIsEnumerable","toLocaleString","toString","valueOf"],enumBugsLength=enumBugProps.length,hasOwnProperty={}.hasOwnProperty,hasEnumBug=!{}.propertyIsEnumerable.call({toString:void 0},"toString");module.exports=enumBug}])});
+var enumBug = function(modules) {
+    function __webpack_require__(moduleId) {
+        if (installedModules[moduleId]) return installedModules[moduleId].exports;
+        var module = installedModules[moduleId] = {
+            exports: {},
+            id: moduleId,
+            loaded: !1
+        };
+        return modules[moduleId].call(module.exports, module, module.exports, __webpack_require__), 
+        module.loaded = !0, module.exports;
+    }
+    var installedModules = {};
+    return __webpack_require__.m = modules, __webpack_require__.c = installedModules, 
+    __webpack_require__.p = "", __webpack_require__(0);
+}([ function(module, exports) {
+    "use strict";
+    function enumBug(obj, callback) {
+        if (hasEnumBug) for (var i = 0; i < enumBugsLength; i += 1) {
+            var prop = enumBugProps[i];
+            hasOwnProperty.call(obj, prop) && callback.call(void 0, prop);
+        }
+    }
+    var enumBugProps = [ "constructor", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "toLocaleString", "toString", "valueOf" ], enumBugsLength = enumBugProps.length, hasOwnProperty = {}.hasOwnProperty, hasEnumBug = !{}.propertyIsEnumerable.call({
+        toString: void 0
+    }, "toString");
+    module.exports = enumBug;
+} ]);
